693397bf1b0a3270d46857ccb729d1a55d13f677
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-10-31 18:04:08 -0700

    finished adding coloring to Past/Present/Future, still need Storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,32 @@
 var container  = document.querySelector(".container")
 var blocks = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+var date = new Date(); 
+
+function isCurrentPastOrFuture(hour) { 
+    if (date.getHours() === hour) { 
+        return 1 
+    } 
+    else if (date.getHours() > hour) { 
+        return 2 // past  
+    } else { 
+        return 3 // future
+    }
+}
 
 for (var i = 0; i < blocks.length; i++) { 
     var block = document.createElement("div"); 
     var wrapper = document.createElement("div"); 
-    wrapper.setAttribute("style", "display: flex; align-items: center; width: 100%;"); 
+    wrapper.setAttribute("class", "row timeblock"); 
     var textbox = document.createElement("textarea"); 
-    textbox.setAttribute("style", "flex-grow: 1; height: 100%;"); 
+    if (isCurrentPastOrFuture(blocks[i]) === 1) { 
+        wrapper.classList.add("present");
+    } else if (isCurrentPastOrFuture(blocks[i]) === 2) { 
+        wrapper.classList.add("past");
+    } else if (isCurrentPastOrFuture(blocks[i]) === 3) { 
+        wrapper.classList.add("future"); 
+    }
+    textbox.setAttribute("class", "description"); 
+    block.setAttribute("class", "hour");
     block.textContent = moment(blocks[i], 'HH').format('hh:mm a');
     wrapper.appendChild(textbox);
     container.appendChild(block); 
